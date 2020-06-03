c253394a6f8a1031b1ae7d26c245e120f2c7b380
Sarah-Marie <62271751+Sarah-Marie@users.noreply.github.com>
2020-06-03 14:03:39 -0400

    made changes

--- ch21_PRSConsoleDB/src/db/UserDB.java
+++ ch21_PRSConsoleDB/src/db/UserDB.java
@@ -7,7 +7,7 @@ import java.util.List;
 import business.User;
 
 public class UserDB implements DAO<User> {
-	
+
 	private Connection getConnection() throws SQLException {
 		String dbURL = "jdbc:mysql://localhost:3306/prs";
 		String username = "prs_user";
@@ -16,37 +16,52 @@ public class UserDB implements DAO<User> {
 		return conn;
 	}
 
-	@Override
-	public User get(int id) {
-		String sql = "SELECT UserName, Password, FirstName, LastName, PhoneNumber, Email, IsReviewer, IsAdmin"
-				+ "From User"
-				+ "Where ID = ?";
+	public User login(String un, String pw) {
+		User u = null;
+		String sql = "SELECT * from User where username = ? and password = ?";
 		try (Connection connection = getConnection();
 				PreparedStatement ps = connection.prepareStatement(sql)) {
-			String UserName;
-			ps.setString(1, UserName);
+			ps.setString(1, un);
+			ps.setString(2, pw);
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
-				String password = rs.getString("Password");
-				String firstName;
-				String lastName;
-				String phoneNumber;
-				String email;
-				User u = new User (UserName, password, firstName, lastName, phoneNumber, email, IsReviewer, IsAdmin, );
-				rs.close();
-				return u;
-			} else {
-				rs.close();
-				return null;
-			}
-		} catch (SQLException e) {
-			System.err.println(e);
-			return null;
-		}
+				u = getUserFromResultSet(rs);
 				
-		
-		
-	}
+			
+			
+				
+		} 
+			else {
+				
+			}
+			rs.close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+		return u;		
+		} 
+	@Override
+
+	public User get(int id) {
+		User u = null;
+		String sql = "SELECT * from User where id = ?";
+		try (Connection connection = getConnection();
+				PreparedStatement ps = connection.prepareStatement(sql)) {
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				u = getUserFromResultSet(rs);
+				
+			}
+			rs.close();
+			
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+			return u;
+		 }
 
 	@Override
 	public List<User> list() {
@@ -54,25 +69,14 @@ public class UserDB implements DAO<User> {
 		List<User> users = new ArrayList<>();
 		try (Connection conn = getConnection();
 				PreparedStatement ps = conn.prepareStatement(sql);
-						ResultSet rs = ps.executeQuery()) {
+				ResultSet rs = ps.executeQuery()) {
 			// while there is a row in the rs
 			while (rs.next()) {
-				int id = rs.getInt(1);
-				String un = rs.getString(2);
-				String pw = rs.getString(3);
-				String fn = rs.getString(4);
-				String ln = rs.getString(5);
-				String pn = rs.getString(6);
-				String e = rs.getString(7);
-				boolean r = rs.getBoolean(8);
-				boolean a = rs.getBoolean(9);
-				User u = new User(id, un, pw, fn, ln, pn, e, r, a);
+				User u = getUserFromResultSet(rs);
 				users.add(u);
-				
-				
-				
+
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -81,52 +85,64 @@ public class UserDB implements DAO<User> {
 		return users;
 	}
 
+	private User getUserFromResultSet(ResultSet rs) throws SQLException {
+		int id = rs.getInt(1);
+		String un = rs.getString(2);
+		String pw = rs.getString(3);
+		String fn = rs.getString(4);
+		String ln = rs.getString(5);
+		String pn = rs.getString(6);
+		String e = rs.getString(7);
+		boolean r = rs.getBoolean(8);
+		boolean a = rs.getBoolean(9);
+		User u = new User(id, un, pw, fn, ln, pn, e, r, a);
+		return u;
+
+	}
+
 	@Override
 	public boolean add(User u) {
 		boolean success = false;
-		String sql = "INSERT into User (UserName, Password, FirstName, LastName, PhoneNumber, " + 
-				"Email, IsReviewer, IsAdmin)" +
-				"values (?, ?, ?, ?, ?, ?, ?, ?)";
-		try (Connection conn = getConnection();
-				PreparedStatement ps = conn.prepareStatement(sql)) {
-			ps.setString(1,  u.getUserName());
-			ps.setString(2,  u.getPassword());
-			ps.setString(3,  u.getFirstName());
-			ps.setString(4,  u.getLastName());
-			ps.setString(5,  u.getPhoneNumber());
-			ps.setString(6,  u.getEmail());
-			ps.setBoolean(7,  u.isReviewer());
-			ps.setBoolean(8,  u.isAdmin());
+		String sql = "INSERT into User (UserName, Password, FirstName, LastName, PhoneNumber, "
+				+ "Email, IsReviewer, IsAdmin)" + "values (?, ?, ?, ?, ?, ?, ?, ?)";
+		try (Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+			ps.setString(1, u.getUserName());
+			ps.setString(2, u.getPassword());
+			ps.setString(3, u.getFirstName());
+			ps.setString(4, u.getLastName());
+			ps.setString(5, u.getPhoneNumber());
+			ps.setString(6, u.getEmail());
+			ps.setBoolean(7, u.isReviewer());
+			ps.setBoolean(8, u.isAdmin());
 			ps.executeUpdate();
 			success = true;
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
 		return success;
 	}
 
 	@Override
-	public boolean update(User u) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean delete(User u) {
+		boolean success = false;
+
+		String sql = "DELETE FROM User where id = ?";
+		try (Connection connection = getConnection(); PreparedStatement ps = connection.prepareStatement(sql)) {
+			ps.setInt(1, u.getId());
+			ps.executeUpdate();
+			success = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return success;
+
 	}
 
 	@Override
-	public boolean delete(User u) {
-		String sql = "DELETE FROM Users"
-				   + "WHERE UserName = ?";
-		try (Connection connection = getConnection();
-				PreparedStatement ps = connection.prepareStatement(sql)) {
-			ps.setString(1,  u.getUserName());
-			ps.executeUpdate();
-			return true;
-			
-		} catch (SQLException e) {
-			System.err.println(e);
-			return false;
-		}
-		
+	public boolean update(User u) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
--- ch21_PRSConsoleDB/src/ui/PRSConsoleApp.java
+++ ch21_PRSConsoleDB/src/ui/PRSConsoleApp.java
@@ -8,24 +8,31 @@ import db.UserDB;
 import util.Console;
 
 public class PRSConsoleApp {
-	
+
 	private static DAO<User> userRepo = new UserDB();
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the PRS");
-		
-		
-		
+
 		String command = "";
 		while (!command.equalsIgnoreCase("exit")) {
 			displayMenu();
-			command = Console.getString("Command:  ",true);
+			command = Console.getString("Command:  ", true);
 			switch (command) {
 			case "list":
 				listUsers();
 				break;
+			case "get":
+				getUser();
+				break;
+			case "login":
+				loginUser();
+				break;
 			case "add":
 				addUser();
+				break;
+			case "delete":
+				deleteUser();
 				break;
 			case "exit":
 				break;
@@ -34,18 +41,53 @@ public class PRSConsoleApp {
 				break;
 			}
 		}
-		
-		
-	
-		
-		
-		
-		
+
 		System.out.println("Bye");
 
 	}
-	
-	
+
+	private static User promptIdAndReturnUser() {
+		User u = null;
+		while (u == null) {
+			int id = Console.getInt("User id?: ", 0, (int) Double.POSITIVE_INFINITY);
+			u = userRepo.get(id);
+			if (u == null) {
+				System.out.println("No user found for id:  " + id + ".");
+				System.out.println();
+			}
+		}
+
+		return u;
+	}
+
+	private static void deleteUser() {
+		User u = promptIdAndReturnUser();
+		if (userRepo.delete(u)) {
+			System.out.println("User deleted.");
+
+		} else {
+			System.out.println("Error deleting user.");
+
+		}
+		System.out.println();
+	}
+
+// TODO Reslove issue with dao.login()
+	private static void loginUser() {
+		System.out.println("Method not yet implemented.");
+		// String un = Console.getString("Username?: ", true);
+		// String pw = Console.getString("Password?: ", true);
+		// User u = userRepo.login
+		// display message
+
+	}
+
+	private static void getUser() {
+		User u = promptIdAndReturnUser();
+		System.out.println("User found: "+u);
+		System.out.println();
+	}
+
 	private static void addUser() {
 		// prompt for all user fields
 		String un = Console.getString("UserName? ", true);
@@ -57,39 +99,34 @@ public class PRSConsoleApp {
 		// skip boolean fields for now
 		User u = new User(un, pw, fn, ln, pn, e, false, false);
 		if (userRepo.add(u)) {
-			
-		
-		System.out.println("User successfully added");
-		}
-		else {
+
+			System.out.println("User successfully added");
+		} else {
 			System.out.println("Issue adding user");
 		}
-		
-	}
 
+	}
 
 	private static void listUsers() {
-	List<User> users = userRepo.list();
-		
+		List<User> users = userRepo.list();
+
 		System.out.println("List of users...");
-		for (User u: users) {
+		for (User u : users) {
 			System.out.println(u);
 		}
-		
-	}
 
+	}
 
 	private static void displayMenu() {
 		System.out.println("Command Menu:");
 		System.out.println("=============");
 		System.out.println("list - list all users");
+		// System.out.println("login - login to app");
+		System.out.println("get - get a user");
 		System.out.println("add - add user");
-		System.out.println("exit - exit");
-		
+		System.out.println("delete - delete a user");
+		System.out.println("exit - exit the app");
+
 	}
-	
-	
-	
-	
 
 }
